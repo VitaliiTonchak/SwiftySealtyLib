ba1aeeec7c9ad85661ebbb5e8df6aacdf717798d
Vitalii Tonchak <vitalii.tonchak@soprasteria.com>
2023-05-22 22:49:41 +0200

    Initial Commit

---
+++ SwiftySealtyLib/SwiftySealtyLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftySealtyLib.h
+//  SwiftySealtyLib
+//
+//  Created by vtonchak on 22/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftySealtyLib.
+FOUNDATION_EXPORT double SwiftySealtyLibVersionNumber;
+
+//! Project version string for SwiftySealtyLib.
+FOUNDATION_EXPORT const unsigned char SwiftySealtyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftySealtyLib/PublicHeader.h>
+
+
