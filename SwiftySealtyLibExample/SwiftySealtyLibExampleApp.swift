4343a2ac90f3db0153d0ba24d8ad10b70a3b724c
Vitalii Tonchak <vitalii.tonchak@soprasteria.com>
2023-05-22 22:56:50 +0200

    init

---
+++ SwiftySealtyLibExample/SwiftySealtyLibExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySealtyLibExampleApp.swift
+//  SwiftySealtyLibExample
+//
+//  Created by vtonchak on 22/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySealtyLibExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
